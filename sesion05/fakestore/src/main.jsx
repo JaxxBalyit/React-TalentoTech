829e03b34e3dde809a1a2eab95e4a34b6f20e103
JaxBalyit <jaxxgamerytmk@gmail.com>
2023-11-17 20:53:05 -0600

    Sesion05 - FakeSstore

--- sesion05/fakestore/src/main.jsx
+++ sesion05/fakestore/src/main.jsx
@@ -2,12 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { TimerProvider } from './context/TimerContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-	<React.StrictMode>
-		<TimerProvider>
-			<App />
-		</TimerProvider>
-	</React.StrictMode>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
